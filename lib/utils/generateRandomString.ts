12bce7d1585700f708217929da0ed230abf013d3
Daniel Rivers <me@danielrivers.com>
2024-09-11 16:46:58 +0100

    fix: make generateRandomString work when not crypto

--- lib/utils/generateRandomString.ts
+++ lib/utils/generateRandomString.ts
@@ -4,11 +4,27 @@
  * @returns {string} required secret
  */
 export const generateRandomString = (length: number = 28): string => {
-  const arr = new Uint8Array(length / 2);
-  crypto.getRandomValues(arr);
-  return Array.from(arr, dec2hex).join("");
+  if (crypto) {
+    const arr = new Uint8Array(length / 2);
+    crypto.getRandomValues(arr);
+    return Array.from(arr, dec2hex).join("");
+  } else {
+    return generateRandomStringNonCrypto(length);
+  }
 };
 
 function dec2hex(dec: number) {
   return dec.toString(16).padStart(2, "0");
+}
+
+function generateRandomStringNonCrypto(length: number = 28) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  
+  return result;
 }
