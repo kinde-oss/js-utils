7245aaaf04737fc8d95e8eaa421ddf0ed180d214
Daniel Rivers <me@danielrivers.com>
2024-09-11 16:49:17 +0100

    fix: ensure expo storage is loaded before using.

--- lib/sessionManager/stores/expoSecureStore.ts
+++ lib/sessionManager/stores/expoSecureStore.ts
@@ -4,6 +4,15 @@ import { splitString } from "../utils.js";
 
 let expoSecureStore: typeof import("expo-secure-store") | undefined = undefined;
 
+async function waitForExpoSecureStore() {
+  let tries = 0;
+  while (!expoSecureStore && tries < 20) {
+    console.log('waiting');
+    await new Promise((resolve) => setTimeout(resolve, 100));
+    tries++;
+  }
+}
+
 /**
  * Provides a expo local store based session manager implementation for the browser.
  * @class ExpoSecureStore
@@ -42,6 +51,7 @@ export class ExpoSecureStore<V = StorageKeys> implements SessionManager<V> {
     itemKey: V | StorageKeys,
     itemValue: unknown,
   ): Promise<void> {
+    await waitForExpoSecureStore();
     // clear items first
     await this.removeSessionItem(itemKey);
 
@@ -66,6 +76,8 @@ export class ExpoSecureStore<V = StorageKeys> implements SessionManager<V> {
    * @returns {unknown | null}
    */
   async getSessionItem(itemKey: V | StorageKeys): Promise<unknown | null> {
+    await waitForExpoSecureStore();
+
     const chunks = [];
     let index = 0;
 
@@ -91,6 +103,8 @@ export class ExpoSecureStore<V = StorageKeys> implements SessionManager<V> {
    * @returns {void}
    */
   async removeSessionItem(itemKey: V | StorageKeys): Promise<void> {
+    await waitForExpoSecureStore();
+
     let index = 0;
 
     let chunk = await expoSecureStore!.getItemAsync(
