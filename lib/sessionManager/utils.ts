c78f029fde370aff5af783bd8f707fb3d2e97bac
Daniel Rivers <me@danielrivers.com>
2024-10-30 15:51:44 +0000

    feat: improve splitString

--- lib/sessionManager/utils.ts
+++ lib/sessionManager/utils.ts
@@ -2,9 +2,5 @@ export function splitString(str: string, length: number): string[] {
   if (length <= 0) {
     return [];
   }
-  const result = [];
-  for (let i = 0; i < str.length; i += length) {
-    result.push(str.slice(i, i + length));
-  }
-  return result;
+  return str.match(new RegExp(`.{1,${length}}`, 'g')) || [];
 }
