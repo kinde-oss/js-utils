69ec9349ee1880194882a0824cd3a635619da005
dtoxvanilla1991 <dtoxvanilla@gmail.com>
2025-06-16 00:39:14 +0100

    feat: redefine session interface in Express namespace for enhanced session management

--- lib/sessionManager/stores/expressSessionManager.ts
+++ lib/sessionManager/stores/expressSessionManager.ts
@@ -1,11 +1,23 @@
 import type {Request} from 'express';
 import {SessionBase, StorageKeys, type SessionManager} from '../types.js';
 
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Express {
+    interface Request {
+      session?: {
+        [key: string]: unknown;
+        destroy: (callback: (err?: Error | null) => void) => void;
+      };
+    }
+  }
+}
+
 /**
  * Provides an Express session-based session manager.
  * This class acts as a structured interface to the 'req.session' object,
  * that is populated by the express-session middleware.
- * @class ExpressSessionManager 
+ * @class ExpressSessionManager
  */
 export class ExpressSessionManager<V extends string = StorageKeys>
   extends SessionBase<V>
@@ -67,7 +79,7 @@ export class ExpressSessionManager<V extends string = StorageKeys>
    */
   async destroySession(): Promise<void> {
     return new Promise((resolve, reject) => {
-      this.req.session!.destroy((err: Error | null) => {
+      this.req.session!.destroy((err) => {
         if (err) {
           //figure out how to handle this better
           console.error("Error destroying session:", err);
