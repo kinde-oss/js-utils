8191ee6e4092477cd267d450e5e44aa154eb352c
Bailey Eaton <xYoshify@gmail.com>
2025-07-17 00:56:30 +1000

    fix: add empty array fallback

--- lib/utils/token/getEntitlements.ts
+++ lib/utils/token/getEntitlements.ts
@@ -21,19 +21,21 @@ export const getEntitlements = async (): Promise<getEntitlementsResponse> => {
   });
   return {
     orgCode: response.org_code,
-    plans: response.plans.map((plan: ApiPlan) => ({
-      key: plan.key,
-      subscribedOn: plan.subscribed_on,
-    })),
-    entitlements: response.entitlements.map((entitlement: ApiEntitlement) => ({
-      id: entitlement.id,
-      fixedCharge: entitlement.fixed_charge,
-      priceName: entitlement.price_name,
-      unitAmount: entitlement.unit_amount,
-      featureKey: entitlement.feature_key,
-      featureName: entitlement.feature_name,
-      entitlementLimitMax: entitlement.entitlement_limit_max,
-      entitlementLimitMin: entitlement.entitlement_limit_min,
-    })),
+    plans:
+      response.plans?.map((plan: ApiPlan) => ({
+        key: plan.key,
+        subscribedOn: plan.subscribed_on,
+      })) || [],
+    entitlements:
+      response.entitlements?.map((entitlement: ApiEntitlement) => ({
+        id: entitlement.id,
+        fixedCharge: entitlement.fixed_charge,
+        priceName: entitlement.price_name,
+        unitAmount: entitlement.unit_amount,
+        featureKey: entitlement.feature_key,
+        featureName: entitlement.feature_name,
+        entitlementLimitMax: entitlement.entitlement_limit_max,
+        entitlementLimitMin: entitlement.entitlement_limit_min,
+      })) || [],
   };
 };
